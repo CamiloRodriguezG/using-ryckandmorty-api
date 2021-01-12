42a15007233e598c5c779ac8e00a1f8145075299
CamiloRodriguezG <72152781+CamiloRodriguezG@users.noreply.github.com>
2021-01-12 17:32:58 -0500

    Logical changes for the new search feature
    Here i add some new lines of functions that make the searcher works

--- rick&morty/index.js
+++ rick&morty/index.js
@@ -1,5 +1,8 @@
 let container = document.getElementById('main');
+let mainButton = document.getElementById('boton');
 let content = '';
+let myNonWhitespaceRegex = /\S/gi;
+let mySpaceRegex = /\s+/g;
 let fn = Math.floor(Math.random()*671);
 const change = () =>{
     let numeroAzar = Math.floor(Math.random()*671);
@@ -34,4 +37,75 @@ const getInfFromApi = (URL) =>{
     })
 }
 
+const getCharacterBysearch = (URLSEARCH) =>{
+    content = '';
+    fetch(URLSEARCH)
+    .then( response => response.json())
+    .then( response => {
+        console.log(response)
+        if(response.error == "There is nothing here"){
+            content+=`
+                <h2>Opps! seems the character you searched doesn't exit</h2>
+            `;
+            container.innerHTML = content;
+        }else{
+            content += `
+                <div id="cards-container">
+            `
+            for(let i=0; i<response.results.length; i++){
+                content+=`
+                    <div class="character-card">
+                        <button class="come-btn" onclick="goToCharacter(${response.results[i].id})"><img class="card-img" src="${response.results[i].image}" alt="${response.results[i].name}"/></button>
+                        <div id="card-inf">
+                            <p class="card-data"><b>${response.results[i].name}</b> 
+                            <p class="card-data">${response.results[i].species} - ${response.results[i].status}</p>
+                            <p class="card-data">Origin: ${response.results[i].origin.name}</p>
+                        </div>
+                    </div>
+                `
+            }
+            content += `
+                </div>
+            `
+            container.style.background = 'rgb(228,167,136)';
+            container.innerHTML = content;
+        }
+    })
+    mainButton.hidden = 'true';
+}
+
+const goToCharacter = id =>{
+    container.style.boxShadow = '0 5px 20px -10px rgba(0,0,0,.75)';
+    content = '';
+    const URL = `https://rickandmortyapi.com/api/character/${id}`
+    container.style.background = '#ddd';
+    getInfFromApi(URL);
+}
+
+const searchCharacter = () => {
+    container.style.boxShadow = 'none';
+    let content = '';
+    let URLSEARCH ='https://rickandmortyapi.com/api/character/';
+    let name = document.getElementById('name-field').value;
+    let species = document.getElementById('species-dropdown').value;
+    let status = document.getElementById('status-dropdown').value;
+    if(myNonWhitespaceRegex.test(name)){
+        if(mySpaceRegex.test(name)){
+            name = name.replace(mySpaceRegex, "%20");
+        }
+        URLSEARCH += `?name=${name}`;
+        if(status != 'none'){
+            URLSEARCH += `&status=${status}`
+        }
+        if(species != 'none'){
+            URLSEARCH += `&species=${species}`
+        }
+        getCharacterBysearch(URLSEARCH);
+    }else{
+        content+=`
+            <h2>Please enter a name before click seacrh</h2>
+        `;
+        container.innerHTML = content;
+    }
+}
 getInfFromApi(URL);
